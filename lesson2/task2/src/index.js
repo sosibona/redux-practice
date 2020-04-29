5ae7c7b438d06aa86d03629b08abf83f20b75983
sosibona <empty.tara@gmail.com>
2020-04-29 17:43:48 +0300

    fix join

--- lesson2/task2/src/index.js
+++ lesson2/task2/src/index.js
@@ -25,7 +25,7 @@ decrementBtn.addEventListener('click', onDecrement)
 store.subscribe(() => {
   const state = store.getState();
   const currentValue = state.history.reduce((acc, value) => acc + Number(value), 0);
-  const historyString = state.history.join(' ');
+  const historyString = state.history.join('');
   resultElem.textContent = state.history.length === 0 
     ? '' 
     : `${historyString} = ${currentValue}`;
