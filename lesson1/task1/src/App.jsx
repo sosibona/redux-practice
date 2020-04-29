32167eeacb994216ade2cd496de63f14d2029d1a
sosibona <empty.tara@gmail.com>
2020-04-29 11:07:01 +0300

    delete coments

--- lesson1/task1/src/App.jsx
+++ lesson1/task1/src/App.jsx
@@ -3,11 +3,8 @@ import React, { Component } from "react";
 class App extends Component {
   render() {
     return (
-      // <!-- App -->
       <div>
-        {/* <!-- ThemedButton --> */}
         <button class="btn">Dynamic Theme</button>
-        {/* <!-- ThemedButton --> */}
         <button class="btn">Default Theme</button>
       </div>
     );
