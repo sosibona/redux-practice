1980e6d465452ad38bcd19c74dc34cb649ff8fea
sosibona <empty.tara@gmail.com>
2020-04-29 16:32:48 +0300

    delete dont use code

--- lesson2/task1/src/index.js
+++ lesson2/task1/src/index.js
@@ -27,8 +27,3 @@ const counterReducer = (state = 0, action) => {
 }
 
 const store = createStore(counterReducer);
-
-store.dispatch(increment())
-store.dispatch(increment())
-store.dispatch(increment())
-store.dispatch(decrement())
