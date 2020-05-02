8c6de8f97d65ab004153d1b16d36ad94b5bc63a5
sosibona <empty.tara@gmail.com>
2020-05-03 00:56:03 +0300

    fix

--- lesson4/task2/src/users/Users.jsx
+++ lesson4/task2/src/users/Users.jsx
@@ -1,33 +1,35 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import * as usersActions from './users.actions'
+import * as usersActions from "./users.actions";
 
 class Users extends Component {
-
   onUserCreate = () => {
     const id = Math.round(Math.random() * 1000000);
     const newUser = {
       id,
-      name: `User # ${id}`
-    }
-    this.props.createUser(newUser)
-  }
-
-  // onDeleteUser = userId => {
-  //   this.props.deleteUser(userId)
-  // }
+      name: `User # ${id}`,
+    };
+    this.props.createUser(newUser);
+  };
 
   render() {
-    const {users, deleteUser} = this.props;
+    const { users, deleteUser } = this.props;
     return (
       <div className="users">
-        <button className="users__create-btn" onClick={this.onUserCreate}>Create user</button>
+        <button className="users__create-btn" onClick={this.onUserCreate}>
+          Create user
+        </button>
         <ul className="users__list">
-          {users.map(user => (
+          {users.map((user) => (
             <li key={user.id} className="users__list-item">
-            <span>{user.name}</span>
-            <button className="users__delete-btn" onClick={() => deleteUser(user.id)}>+</button>
-          </li>
+              <span>{user.name}</span>
+              <button
+                className="users__delete-btn"
+                onClick={() => deleteUser(user.id)}
+              >
+                +
+              </button>
+            </li>
           ))}
         </ul>
       </div>
@@ -35,16 +37,16 @@ class Users extends Component {
   }
 }
 
-const mapState = state => {
+const mapState = (state) => {
   return {
-    users: state.users.usersList
-  }
-}
+    users: state.users.usersList,
+  };
+};
 
 const mapDispatch = {
   createUser: usersActions.addUser,
   deleteUser: usersActions.deleteUser,
-}
+};
 
 const connector = connect(mapState, mapDispatch);
 
