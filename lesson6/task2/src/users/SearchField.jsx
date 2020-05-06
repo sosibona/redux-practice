e4d8c6608384f5a4dd35cef3224ec455f4c0c775
sosibona <empty.tara@gmail.com>
2020-05-06 13:52:53 +0300

    fix2

--- lesson6/task2/src/users/SearchField.jsx
+++ lesson6/task2/src/users/SearchField.jsx
@@ -14,14 +14,14 @@ class SearchField extends Component {
     })
   }
 
-  handleUserSerch = () => {
+  handleUserSearch = () => {
     this.props.fetchUserData(this.state.userName)
   }
   render() {
     return (
       <div className="name-form">
         <input type="text" className="name-form__input" value={this.state.userName} onChange={this.onChange} />
-        <button className="name-form__btn btn" onClick={this.handleUserSerch}>Show</button>
+        <button className="name-form__btn btn" onClick={this.handleUserSearch}>Show</button>
       </div>
     );
   }
