b8197007d4db59270150ee1ef9f1d99d11a6ab2b
sosibona <empty.tara@gmail.com>
2020-05-06 13:58:21 +0300

    fix4

--- lesson6/task2/src/App.jsx
+++ lesson6/task2/src/App.jsx
@@ -5,23 +5,12 @@ import { Provider } from "react-redux";
 import store from "./store";
 
 class App extends Component {
-  state = {
-    userData: null,
-  };
-
-  onShowUser = (userName) => {
-    this.setState({
-      isFetching: true,
-    });
-    
-  };
   render() {
     return (
       <Provider store={store}>
         <div className="page">
-          {/* <Spinner /> */}
           <UserInfo />
-          <SearchField onShowUser={this.onShowUser} />
+          <SearchField />
         </div>
       </Provider>
     );
