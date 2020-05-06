d0eeb5f38f47aaac0bcc41593a6813550e8a7277
sosibona <empty.tara@gmail.com>
2020-05-06 16:02:00 +0300

    fix

--- lesson6/hw1/src/weather/weather.reducer.js
+++ lesson6/hw1/src/weather/weather.reducer.js
@@ -2,18 +2,11 @@ import { SHOW_WEATHER, WHEATHER_DATA_RECIEVED } from "./weather.actions";
 
 
 const initialState = {
-  isWeatherData: false,
   weatherData: null,
 }
 
 export const weatherReducer = (state = initialState, action) => {
   switch (action.type) {
-    case SHOW_WEATHER: {
-      return {
-        ...state,
-        isWeatherData
-      }
-    }
     case WHEATHER_DATA_RECIEVED: {
       return {
         ...state,
